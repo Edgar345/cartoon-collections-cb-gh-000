32c7b2e1dfbf2385f5267155c02535addb5213bc
Edgar V <evenegas9@ucmerced.edu>
2019-09-08 09:52:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,12 @@ def long_planeteer_calls array
 end
 
 def find_the_cheese strings_arr
-
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each do |cheese_type|
+    if strings_arr.include? cheese_type
+      return cheese_type
+    end
+  end
+  return nil
 end
